497d796c31ad074765b733f709a0d55ad3835c1b
smulyono <smulyono@me.com>
2014-05-11 00:08:03 -0700

    use compression

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 var express = require('express');
+var compress = require('compression');
 var app = express();
 
-app.use("/", express.static(__dirname));
+// compress
+app.use(compress());
 
-// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+// route to static file
+app.serve = function(dir, path){
+    app.use("/", express.static(path));
+};
 
 
 module.exports = app;
